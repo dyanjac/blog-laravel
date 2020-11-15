3b135fa3e2d2325dea63def94015202dfcdff068
Andyc <andy.jca@outlook.com>
2020-11-15 00:10:04 -0500

    Agregando h3 en el branch Texto

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,6 +10,7 @@ if($_REQUEST){
 	echo "Facebook ";
 	echo "Facebook Nuevo Cambio";
 	echo '<h1 style="color:blue;backgorund-color:green;" >Facebook Nuevo Cambio<h1>';
+	echo '<h3 style="color:blue;backgorund-color:green;" >Facebook Nuevo Cambio<h3>';
 
 }
 
